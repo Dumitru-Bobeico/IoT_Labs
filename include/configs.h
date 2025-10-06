290288638c42a19704bd705e84aed4ce14955353
Dumitru-Bobeico <dima.bobeico.2003@gmail.com>
2025-10-06 16:01:56 +0300

    backup

--- include/configs.h
+++ include/configs.h
@@ -3,20 +3,20 @@
 
 #include <Arduino.h>
 
-#define COMMAND_MAX_LENGTH      10
+#define COMMAND_MAX_LENGTH              10
 
-#define ACTION_MAX_LENGTH       10
+#define ACTION_MAX_LENGTH               10
 
-#define LED_PIN                 13
+#define LED_PIN                         13
 
-#define SERIAL_BAUD             115200
+#define SERIAL_BAUD                     115200
 
-#define I2C_DISPLAY_ADDRESS     0x27
-#define I2C_DISPLAY_COLUMNS     16
-#define I2C_DISPLAY_ROWS        2
+#define I2C_DISPLAY_ADDRESS             0x27
+#define I2C_DISPLAY_COLUMNS             16
+#define I2C_DISPLAY_ROWS                2
 
-#define KEYPAD_ROWS             4
-#define KEYPAD_COLS             4
+#define KEYPAD_ROWS                     4
+#define KEYPAD_COLS                     4
 
 enum KEYPAD_KEYS {
     KEYPAD_PIN_1 = 23,
@@ -29,26 +29,26 @@ enum KEYPAD_KEYS {
     KEYPAD_PIN_8 = 37
 };
 
-#define RED_LED_PIN             54
-#define GREEN_LED_PIN           57
-#define ACCESS_PIN              "1234"
+#define RED_LED_PIN                     54
+#define GREEN_LED_PIN                   57
+#define ACCESS_PIN                      "1234"
 
-#define FIRST_LED               13
-#define SECOND_LED              12
-#define ON_OFF_BUTTON_PIN       10
-#define UP_BUTTON_PIN           9
-#define DOWN_BUTTON_PIN         8  
-#define DEBOUNCE_TIME_MS        200
+#define FIRST_LED                       13
+#define SECOND_LED                      12
+#define ON_OFF_BUTTON_PIN               10
+#define UP_BUTTON_PIN                   9
+#define DOWN_BUTTON_PIN                 8  
+#define DEBOUNCE_TIME_MS                200
 
-#define SECOND_LED_BASE_FREQUENCY_MS 1
-#define SECOND_LED_FREQUENCY_STEP_MS 1
-#define FIRST_TASK_OFFSET_MS        1
-#define FIRST_TASK_RECCURENCE_MS    10
+#define SECOND_LED_BASE_FREQUENCY_MS    1
+#define SECOND_LED_FREQUENCY_STEP_MS    1
+#define FIRST_TASK_OFFSET_MS            1
+#define FIRST_TASK_RECCURENCE_MS        10
 
-#define SECOND_TASK_OFFSET_MS   5
-#define SECOND_TASK_RECCURENCE_MS   10
+#define SECOND_TASK_OFFSET_MS           5
+#define SECOND_TASK_RECCURENCE_MS       10
 
-#define THIRD_TASK_OFFSET_MS    10
-#define THIRD_TASK_RECCURENCE_MS    50
+#define THIRD_TASK_OFFSET_MS            10
+#define THIRD_TASK_RECCURENCE_MS        50
 
 #endif
