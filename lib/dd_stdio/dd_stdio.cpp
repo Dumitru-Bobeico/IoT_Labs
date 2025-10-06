538566c88e85f5437503ae72738931bc5eaac01c
Dumitru-Bobeico <dima.bobeico.2003@gmail.com>
2025-10-06 22:33:52 +0300

    backup

--- lib/dd_stdio/dd_stdio.cpp
+++ lib/dd_stdio/dd_stdio.cpp
@@ -2,37 +2,19 @@
 
 static FILE serial_stdout;
 
-LiquidCrystal_I2C lcd(I2C_DISPLAY_ADDRESS, I2C_DISPLAY_COLUMNS, I2C_DISPLAY_ROWS);
+int my_putchar(char c, FILE* f) {
+    Serial.write(c);
+    return 0;
+}
 
-char keys[KEYPAD_ROWS][KEYPAD_COLS] = 
-{
-    {'1','2','3', 'A'},
-    {'4','5','6', 'B'},
-    {'7','8','9', 'C'},
-    {'*','0','#', 'D'}
-};
-byte rowPins[KEYPAD_ROWS] = {KEYPAD_PIN_1, KEYPAD_PIN_2, KEYPAD_PIN_3, KEYPAD_PIN_4};
-byte colPins[KEYPAD_COLS] = {KEYPAD_PIN_5, KEYPAD_PIN_6, KEYPAD_PIN_7, KEYPAD_PIN_8};
-
-Keypad kpd = Keypad( makeKeymap(keys), rowPins, colPins, KEYPAD_ROWS, KEYPAD_COLS );
-
-int lcd_my_putchar(char c, FILE* f) {
-    lcd.write(c);
+int my_getchar(FILE* f) {
+    while (!Serial.available());
+    char c = Serial.read();
+    Serial.write(c);
     return c;
 }
 
-int keypad_my_getchar(FILE* f) {
-    char key = NO_KEY;
-    while (key == NO_KEY) {
-        key = kpd.getKey();
-    }
-    return key;
-}
-
 void stdio_init() {
-    lcd.init();
-    lcd.backlight();
-
-    fdev_setup_stream(&serial_stdout, lcd_my_putchar, keypad_my_getchar, _FDEV_SETUP_RW);
+    fdev_setup_stream(&serial_stdout, my_putchar, my_getchar, _FDEV_SETUP_RW);
     stdin = stdout = &serial_stdout;
 }
--- lib/dd_stdio/dd_stdio.h
+++ lib/dd_stdio/dd_stdio.h
@@ -4,13 +4,8 @@
 #include <stdio.h>
 #include <Arduino.h>
 
-#include "LiquidCrystal_I2C.h"
-#include "Keypad.h"
-
 #include "configs.h"
 
 void stdio_init();
-
-extern LiquidCrystal_I2C lcd;
 
 #endif
