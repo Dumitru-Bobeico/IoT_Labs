aa99b14d8e37b903352a4d8ce6bb18013fce8ea5
Dumitru-Bobeico <dima.bobeico.2003@gmail.com>
2025-10-28 11:44:32 +0200

    fix

--- lib/tasks/tasks.cpp
+++ lib/tasks/tasks.cpp
@@ -23,16 +23,26 @@ void vTaskButtonLed(void *pvParameters)
 {
     static bool ledActive = false;
     static unsigned long ledOnTime = 0;
+    static bool buttonPrevState = false;
+    static unsigned long lastPressTime = 0;
 
     for (;;)
     {
-        if (is_button_pressed(ON_OFF_BUTTON_PIN))
+        bool pressed = is_button_pressed(ON_OFF_BUTTON_PIN);
+
+        // Debounce 200ms, trigger once per press
+        if (pressed && !buttonPrevState && (millis() - lastPressTime > 200))
         {
+            lastPressTime = millis();
             led_set(FIRST_LED, true);
             ledActive = true;
             ledOnTime = millis();
+
             xSemaphoreGive(xButtonSemaphore);
+            printf("[Task 1] Semaphore given\r\n");
         }
+
+        buttonPrevState = pressed;
 
         if (ledActive && (millis() - ledOnTime >= LED_PRESSED_DURATION_MS))
         {
@@ -53,6 +63,8 @@ void vTaskSincron(void *pvParameters)
     {
         if (xSemaphoreTake(xButtonSemaphore, portMAX_DELAY) == pdTRUE)
         {
+            printf("[Task 2] Semaphore detected\r\n");
+
             N++;
             if (N > 50) N = 1; // prevent overflow
 
@@ -60,12 +72,19 @@ void vTaskSincron(void *pvParameters)
             for (int i = 0; i < N; i++)
                 buffer[i] = i + 1;
 
+            // Print buffer content
+            printf("[Task 2] Buffer content: ");
+            for (int i = 0; i < N; i++)
+                printf("%d%s", buffer[i], (i < N-1) ? ", " : "\r\n");
+
             // Add zero terminator
             buffer[N] = 0;
 
             // Send to queue
             for (int i = 0; i <= N; i++)
                 xQueueSend(xBufferQueue, &buffer[i], portMAX_DELAY);
+
+            printf("[Task 2] Queue sent\r\n");
 
             // Blink LED N times
             for (int i = 0; i < N; i++)
@@ -81,6 +100,7 @@ void vTaskSincron(void *pvParameters)
     }
 }
 
+// --- Task 3: Asincron ---
 void vTaskAsincron(void *pvParameters)
 {
     uint8_t byte;
@@ -90,15 +110,19 @@ void vTaskAsincron(void *pvParameters)
     {
         while (xQueueReceive(xBufferQueue, &byte, 0) == pdTRUE)
         {
-            if (newSeries) {
-                printf("\r\n[AsincronTask] Serie nouă: ");
+            if (newSeries)
+            {
+                printf("\r\n[Task 3] Serie nouă: ");
                 newSeries = false;
             }
 
-            if (byte == 0) {
+            if (byte == 0)
+            {
                 printf("\r\n");
                 newSeries = true;
-            } else {
+            }
+            else
+            {
                 printf("%d ", byte);
             }
         }
@@ -106,7 +130,6 @@ void vTaskAsincron(void *pvParameters)
         vTaskDelay(pdMS_TO_TICKS(ASINCRON_TASK_DELAY_MS));
     }
 }
-
 
 // --- Task initialization ---
 void tasks_init()
